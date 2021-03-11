73bb66b8e0b0494e7896be969f513bc20b38c30e
Noob-Bot-1 <devbansal1512@gmail.com>
2021-03-11 22:44:46 +0530

    jn

--- Backend/index.js
+++ Backend/index.js
@@ -2,3 +2,10 @@ const express = require("express");
 
 const app = express();
 
+const PORT = process.env.PORT||5000;
+
+app.listen(PORT, () => console.log('Server started on port: ${PORT}'));
+
+app.get("/test",(req,res) =>(
+    res.send("It works")
+));

961611bd79cb4fdac61365ba0f865424449ff239
Noob-Bot-1 <devbansal1512@gmail.com>
2021-03-13 16:14:48 +0530

    Register new Student done

--- Backend/routers/studentRouter.js
+++ Backend/routers/studentRouter.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
+const Student = require("../models/studentModel");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-router.post("/", (req,res) => {
+router.post("/",async (req,res) => {
     try{
         const{email, password, passwordVerify} = req.body;
 
@@ -9,7 +12,7 @@ router.post("/", (req,res) => {
                .status(400)
                .json({errorMessage: "Please enter all details"});
         
-        if(password.lenght <= 8)
+        if(password.length <= 8)
             return res
                .status(400)
                .json({errorMessage: "Please enter a password of more than 8 characters"});
@@ -18,6 +21,32 @@ router.post("/", (req,res) => {
             return res
                 .status(400)
                 .json({errorMessage: "Please enter the same password twice"});
+
+        const existingStudent = await Student.findOne({email});
+            if(existingStudent)
+                return res
+                    .status(400)
+                    .json({errorMessage: "An account with this email already exists"});
+
+
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password,salt);
+
+        const newStudent = new Student({
+            email,passwordHash
+        });
+
+        const savedStudent = await newStudent.save();
+
+        const token = jwt.sign({
+            student : savedStudent._id
+        }, process.env.JWT_SECRET);
+
+        res
+            .cookie("token", token, {
+                httpOnly: true,
+            })
+            .send();
 
     }catch(err) {
         console.error(err);

32ffb6d5993b4f0f156e6c943c9d2f2dee13be1d
Noob-Bot-1 <devbansal1512@gmail.com>
2021-03-13 16:45:10 +0530

    Backend of Student Authentication done

--- Backend/routers/studentRouter.js
+++ Backend/routers/studentRouter.js
@@ -3,10 +3,12 @@ const Student = require("../models/studentModel");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-router.post("/",async (req,res) => {
+//register
+router.post("/registerStudent",async (req,res) => {
     try{
         const{email, password, passwordVerify} = req.body;
 
+        //validation
         if(!email || !password || !passwordVerify)
             return res
                .status(400)
@@ -28,20 +30,23 @@ router.post("/",async (req,res) => {
                     .status(400)
                     .json({errorMessage: "An account with this email already exists"});
 
-
+        //encryption
         const salt = await bcrypt.genSalt();
         const passwordHash = await bcrypt.hash(password,salt);
 
+        //Storing in database
         const newStudent = new Student({
             email,passwordHash
         });
 
         const savedStudent = await newStudent.save();
 
+        //Making a token for more security
         const token = jwt.sign({
             student : savedStudent._id
         }, process.env.JWT_SECRET);
 
+        //sending token in a HTTP-Only cookie(better than local storage)
         res
             .cookie("token", token, {
                 httpOnly: true,
@@ -52,6 +57,59 @@ router.post("/",async (req,res) => {
         console.error(err);
         res.status(500).send();
     }
+});
+
+//log-in
+
+router.post("/loginStudent",async (req,res) => {
+    try{
+        const{email, password} = req.body;
+
+        if(!email || !password)
+            return res 
+                .status(400)
+                .json({errorMessage: "Please enter all the fields."});
+
+        const existingStudent = await Student.findOne({email});
+        if (!existingStudent)
+            return res
+                .status(400)
+                .json({errorMessage: "Wrong email"});
+
+        const passwordCorrect = await bcrypt.compare(password,existingStudent.passwordHash);
+
+        if(!passwordCorrect)
+            return res
+                .status(400)
+                .json({errorMessage: "Wrong password"});
+    
+        //Making a token for more security
+        const token = jwt.sign({
+            student : existingStudent._id
+        }, process.env.JWT_SECRET);
+
+        //sending token in a HTTP-Only cookie(better than local storage)
+        res
+            .cookie("token", token, {
+                httpOnly: true,
+            })
+            .send();
+        
+    }catch(err) {
+        console.error(err);
+        res.status(500).send();
+    }
+});
+
+//log-out
+
+router.get("/logoutStudent", (req,res) =>{
+    res
+        .cookie("token","",{
+            httpOnly: true,
+            expires: new Date(0)
+        })
+        .send();
 });
 
 module.exports = router;

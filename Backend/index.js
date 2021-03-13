1a97cbdfad1f56716b3232d81710a3432027bb3b
Noob-Bot-1 <devbansal1512@gmail.com>
2021-03-13 22:35:37 +0530

    More authentication at backend

--- Backend/index.js
+++ Backend/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const dotenv = require("dotenv").config();
+const cookieParser = require("cookie-parser");
 
 const PORT = 5000;
-
+// app.use();
 app.use(express.json());
+app.use(cookieParser());
 
 mongoose.connect(process.env.MDB_CONNECT, {
     useNewUrlParser: true,
@@ -14,5 +16,7 @@ mongoose.connect(process.env.MDB_CONNECT, {
 
 
 app.use("/auth", require("./routers/studentRouter"));
+
+app.use("/hi", require("./routers/tRouter"));
 
 app.listen(PORT, () => console.log('Server started on port:'+ PORT));

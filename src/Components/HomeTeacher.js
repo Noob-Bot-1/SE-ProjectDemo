8b9b588e547a154ac940672330b890a20bde0fba
maitryy <maitryjadiya123@gmail.com>
2021-04-03 13:42:12 +0530

    partial changes in home page css

--- src/Components/HomeTeacher.js
+++ src/Components/HomeTeacher.js
@@ -1,4 +1,158 @@
 
+// import React, { useContext , useEffect, useState } from 'react'
+// import { Link } from 'react-router-dom';
+// import '../bootstrap/bootstrap.css'
+// import userContext from '../Context/UserContext'
+// import './HomeStudent.css'
+
+// function HomeTeacher() {
+
+//     const [data,setData] = useState([]);
+
+//     useEffect(async() => {
+//         const response = await fetch("http://localhost:5000/course/GetCoreCourses");
+//         const da = await response.json();
+//         setData(da);
+//     }, []);
+//     useEffect( () => {
+//         console.log(data);
+//     }, [data]);
+
+//     const {userName} = useContext(userContext);
+//     return (
+//         <div className='hometeacher container' >
+
+//             <div className="jumbotron teacher">
+//                 <h1 className="display-4">Hello, {userName}!!</h1>
+//                 <p className="lead"> “It’s the teacher that makes the difference, not the classroom.”</p>
+//                 <hr className="my-4"/>
+//                 <strong>
+//                 <p>Enrolled in courses: 4</p>
+//                 </strong>
+//                 <p className="lead">
+//                     <Link to = '/AddCourse' >Add Course</Link>
+//                 </p>
+//             </div>
+            
+//             <div className="card bg-dark text-white enroll-card">
+//                 <img className=" enroll-card-img" src="https://images.unsplash.com/photo-1579547621309-5e57ab324182?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80" alt="Card image"/>
+//                 <div className="card-img-overlay">
+//                     <h1 className=" enroll-card-title card-title">Owned Courses</h1>
+//                 </div>
+//             </div>
+            
+            
+//             <div className="All-courses row row-cols-3">
+           
+//             {data.map(course=>{
+//                 return(
+//                     <div key = '_id'>
+//                         <div className= "col-lg-4 col-md-6 col-sm-12 col-12">
+//                         <div className='card courses mask '>
+//                                 <div className="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+//                                     <a href="#" >
+//                                         <img src="https://www.futureelectronics.com/medias/sys_master/images/images/9601962868766/CMSHEROShapingTheFuture1200x450-D.jpg" className="img-fluid card-img-top"/>
+//                                     </a>
+//                                 </div>
+//                                 <div class="card-body">
+//                                         <p>{ course._id}</p>
+//                                         <h4 className= " card-title">{ course.name}</h4>
+//                                         {/* <h5 className = "card-subtitle text-muted">Amarnath Yadav</h5> */}
+//                                         <p className="text-muted">Credits:{ course.credits}</p>
+//                                         <p className="card-text">{ course.description }</p>
+//                                         <Link  to= {`/course/${course._id}`} >
+//                                             <button className="btn btn-primary btn-course">Edit Course</button>
+                            
+//                                         </Link>
+//                                 </div>
+//                         </div>
+//                         </div>  
+//                     </div>
+                    
+//                 )
+//             })}
+           
+//            </div>
+
+//            <div className="card bg-dark text-white enroll-card">
+//                 <img className=" enroll-card-img" src="https://images.unsplash.com/photo-1579547621309-5e57ab324182?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80" alt="Card image"/>
+//                 <div className="card-img-overlay">
+//                     <h1 className=" enroll-card-title card-title">Explore Courses</h1>
+//                 </div>
+//             </div>
+
+//            <div className="explore-cards">
+//             <div className="container-fluid">
+//                 <div className="row">
+//                     <div className="col-12 mt-3">
+//                         <div className="card">
+//                             <Link to = '/CoreCourses'>
+//                             <div className="card-horizontal">
+//                                 <div className="img-square-wrapper">
+//                                     <img className="" src="https://images.unsplash.com/photo-1563729831178-d09061d83b12?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=634&q=80" alt="Card image cap"/>
+//                                 </div>
+//                                 <div className="card-body">
+//                                     <h4 className="card-title explore-title">Core Courses</h4>
+//                                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//                                 </div>
+//                             </div>
+//                             </Link>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//             <div className="container-fluid">
+//                 <div className="row">
+//                     <div className="col-12 mt-3">
+//                         <div className="card">
+//                             <Link to = '/TechnicalElectiveCourses'>
+//                             <div className="card-horizontal">
+//                                 <div className="card-body">
+//                                     <h4 className="card-title explore-title">Technical Courses</h4>
+//                                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//                                 </div>
+//                                 <div className="img-square-wrapper">
+//                                     <img className="" src="https://images.unsplash.com/photo-1606337321936-02d1b1a4d5ef?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80" alt="Card image cap"/>
+//                                 </div>
+//                             </div>
+//                             </Link>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//             <div className="container-fluid">
+//                 <div className="row">
+//                     <div className="col-12 mt-3">
+//                         <div className="card">
+//                             <Link to = '/NonTechnicalElectiveCourses'>
+//                             <div className="card-horizontal">
+//                                 <div className="img-square-wrapper">
+//                                     <img className="" src="https://images.unsplash.com/photo-1589254066213-a0c9dc853511?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"/>
+//                                 </div>
+//                                 <div className="card-body">
+//                                     <h4 className="card-title explore-title">Non-Technical  Courses</h4>
+//                                     <p className="card-text"> Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//                                 </div>
+//                             </div>
+//                             </Link>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+
+            
+
+//            </div>
+//         </div>
+
+
+
+//     )
+// }
+
+// export default HomeTeacher
+
+
 import React, { useContext , useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 import '../bootstrap/bootstrap.css'
@@ -42,10 +196,11 @@ function HomeTeacher() {
             </div>
             
             
-            <div className="All-courses row row-cols-3">
+            
            
             {data.map(course=>{
                 return(
+                    <div className="All-courses row row-cols-3">
                     <div key = '_id'>
                         <div className= "col-lg-4 col-md-6 col-sm-12 col-12">
                         <div className='card courses mask '>
@@ -60,7 +215,7 @@ function HomeTeacher() {
                                         {/* <h5 className = "card-subtitle text-muted">Amarnath Yadav</h5> */}
                                         <p className="text-muted">Credits:{ course.credits}</p>
                                         <p className="card-text">{ course.description }</p>
-                                        <Link  to= {`/course/${course._id}`} >
+                                        <Link  to= {`/courseTeacher/${course._id}`} >
                                             <button className="btn btn-primary btn-course">Edit Course</button>
                             
                                         </Link>
@@ -68,11 +223,11 @@ function HomeTeacher() {
                         </div>
                         </div>  
                     </div>
-                    
+                    </div>
                 )
             })}
            
-           </div>
+           
 
            <div className="card bg-dark text-white enroll-card">
                 <img className=" enroll-card-img" src="https://images.unsplash.com/photo-1579547621309-5e57ab324182?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80" alt="Card image"/>
@@ -151,3 +306,4 @@ function HomeTeacher() {
 }
 
 export default HomeTeacher
+
--- src/Router.js
+++ src/Router.js
@@ -1,3 +1,101 @@
+// import React, { useContext } from 'react';
+// import {BrowserRouter, Switch, Route} from 'react-router-dom';
+// import LoginStudent from './Auth/LoginStudent';
+// import Register from './Auth/Register';
+// import Footer from './Components/footer';
+// import Header from './Components/Header';
+// import LandingPage from './Components/landingpage';
+// import AuthContext from './Context/AuthContext';
+// import Course from './Components/Course';
+// import HomeTeacher from './Components/HomeTeacher';
+// import HomeStudent from './Components/HomeStudent';
+// import CourseTeacher from './Components/CourseTeacher';
+// import userContext from './Context/UserContext';
+// import AddCourses from './Courses/AddCourses';
+// import CoreCoursePage from './Components/ExploreCourses/CoreCoursePage';
+// import NonTechnicalElectiveCoursePage from './Components/ExploreCourses/NonTechnicalElectiveCoursePage';
+// import TechnicalElectiveCoursePage from './Components/ExploreCourses/TechnicalElectiveCoursePage';
+
+// function Router() {
+//     const {loggedIn} = useContext(AuthContext);
+//     const {user} = useContext(userContext)
+
+//     return (
+//         <BrowserRouter>
+//             <Header />
+//             <Switch>
+//                 <Route exact path = '/'>
+//                     <LandingPage />
+//                 </Route>
+
+//                 <Route exact path = '/course'>
+//                     <Course />
+//                 </Route>
+
+//                 <Route exact path = '/courseTeacher'>
+//                     <CourseTeacher />
+//                 </Route>
+
+//                 <Route exact path = '/HomeStudent'>
+//                     <HomeStudent />
+//                 </Route>
+
+
+//                 {!loggedIn && (
+//                     <>
+//                         <Route exact path = '/Register'>
+//                             <Register />
+//                         </Route>
+//                         <Route exact path = '/Login'>
+//                             <LoginStudent />
+//                         </Route>
+//                     </>
+//                 )}
+
+//                 {loggedIn &&
+//                     <>
+//                         <Route exact path = '/courseid'>
+//                             <Course />
+//                         </Route>
+//                         <Route exact path = '/CoreCourses'>
+//                             <CoreCoursePage />    
+//                         </Route>
+//                         <Route exact path = '/NonTechnicalElectiveCourses'>
+//                             <NonTechnicalElectiveCoursePage />
+//                         </Route>
+//                         <Route exact path = '/TechnicalElectiveCourses'>
+//                             <TechnicalElectiveCoursePage />
+//                         </Route>
+
+//                         { user === "Student" &&
+//                             <>
+//                                 <Route exact path = '/HomeStudent'>
+//                                     <HomeStudent />
+//                                 </Route>
+//                             </>
+//                         }
+
+//                         { user === "Teacher" &&
+//                             <>
+//                                 <Route exact path = '/HomeTeacher'>
+//                                     <HomeTeacher />
+//                                 </Route>
+//                                 <Route exact path = '/AddCourse'>
+//                                     <AddCourses />
+//                                 </Route>
+//                             </>
+//                         }
+
+//                     </>
+//                 } 
+//             </Switch>
+//             <Footer />
+//         </BrowserRouter>
+    
+// )}
+
+// export default Router
+
 import React, { useContext } from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import LoginStudent from './Auth/LoginStudent';
@@ -32,9 +130,11 @@ function Router() {
                     <Course />
                 </Route>
 
-                <Route exact path = '/courseTeacher'>
+                <Route path="/courseTeacher/:id" render={(props) => <CourseTeacher  {...props} />} />
+
+                {/* <Route exact path = '/courseTeacher'>
                     <CourseTeacher />
-                </Route>
+                </Route> */}
 
                 <Route exact path = '/HomeStudent'>
                     <HomeStudent />
